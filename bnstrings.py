ff5db7b94ddfc7fcff855b28e6d0fc902f2d23dc
Antonin Deniau <Antonin-Deniau@users.noreply.github.com>
2018-02-27 16:57:06 +0100

    Update bnstrings.py

--- bnstrings.py
+++ bnstrings.py
@@ -1,21 +1,30 @@
 from binaryninja import *
-import r2pipe
+from binaryninja.enums import SymbolType
+import r2pipe, base64
 
 def spawn(bv):
 	class Task(BackgroundTaskThread):
 		def run(self):
 			r2 = r2pipe.open(bv.file.filename.replace(".bndb", ""))
 
+			loaded = 0
 			r2.cmd("aaaa")
-			strings = r2.cmdj("izzj~{{string}}")
+			stri = r2.cmdj("izj")
 			r2.quit()
 
-			for string in strings:
-				typ = bv.parse_type_string("char[{}]".format(string["size"]))[0]
-				addr = int(string["paddr"])
-				bv.define_user_data_var(addr, typ)
+			for string in stri:
+				addr = int(string["vaddr"])
+				name = base64.b64decode(string["string"])
+				size = string["size"]
 
-			log_info("{} string(s) loaded".format(len(strings)))
+				symbol = Symbol(SymbolType.DataSymbol, addr, name)
+				bv.define_user_symbol(symbol)
+
+				t = bv.parse_type_string("char[{}]".format(size))
+				bv.define_user_data_var(addr, t[0])
+				loaded += 1
+	
+			log_info("{} string(s) loaded".format(loaded))
 
 	task = Task()
 	task.start()
