7d320e613b0db149d51e25bf493de47e022cd2ad
Antonin Deniau <Antonin-Deniau@users.noreply.github.com>
2018-02-27 16:06:55 +0100

    Update bnstrings.py

--- bnstrings.py
+++ bnstrings.py
@@ -2,17 +2,22 @@ from binaryninja import *
 import r2pipe
 
 def spawn(bv):
-  r2 = r2pipe.open(bv.file.filename.replace(".bndb", ""))
+	class Task(BackgroundTaskThread):
+		def run(self):
+			r2 = r2pipe.open(bv.file.filename.replace(".bndb", ""))
 
-  r2.cmd("aaaa")
-  strings = r2.cmdj("izzj")
-  r2.quit()
+			r2.cmd("aaaa")
+			strings = r2.cmdj("izzj~{{string}}")
+			r2.quit()
 
-  log_info("{} string(s) loaded".format(len(strings)))
+			for string in strings:
+				typ = bv.parse_type_string("char[{}]".format(string["size"]))[0]
+				addr = int(string["paddr"])
+				bv.define_user_data_var(addr, typ)
 
-  for string in strings:
-    typ = bv.parse_type_string("char[{}]".format(string["size"]))[0]
-    addr = int(string["paddr"])
-    bv.define_user_data_var(addr, typ)
+			log_info("{} string(s) loaded".format(len(strings)))
+
+	task = Task()
+	task.start()
 
 PluginCommand.register("Find strings", "Find strings with radare2", spawn)
